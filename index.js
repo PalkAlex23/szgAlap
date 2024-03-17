1996af119b83b952749f95ac144d218abb08f204
PalkAlex23 <alexpytmusic@gmail.com>
2024-03-17 12:51:40 +0100

    Nem ir bele operatort a szg elejere

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function gombEsemeny() {
     const gombELEMEK = document.querySelectorAll(".szamok button")
     for (let i = 0; i < 10; i++) {
         gombELEMEK[i].addEventListener("click", function() {
-            const eredmeny = document.querySelector(".kijelzo")
+            const eredmeny = document.querySelector(".kifejezes")
             eredmeny.innerHTML += i
             ertek = true
         })
@@ -24,43 +24,67 @@ function gombEsemeny() {
 function muvjelEsemeny() {
     const osszeadas = document.querySelector("#osszeadas")
     osszeadas.addEventListener("click", function() {
-        const eredmeny = document.querySelector(".kijelzo")
-        eredmeny.innerHTML += osszeadas.innerHTML
+        const eredmeny = document.querySelector(".kifejezes")
+        if (eredmeny.innerHTML === '') {
+            eredmeny.innerHTML = ""
+        } else {
+            eredmeny.innerHTML += osszeadas.innerHTML
+        }
     })
 
     const kivonas = document.querySelector("#kivonas")
     kivonas.addEventListener("click", function() {
-        const eredmeny = document.querySelector(".kijelzo")
-        eredmeny.innerHTML += kivonas.innerHTML
+        const eredmeny = document.querySelector(".kifejezes")
+        if (eredmeny.innerHTML === '') {
+            eredmeny.innerHTML = ""
+        } else {
+            eredmeny.innerHTML += kivonas.innerHTML
+        }
     })
 
     const szorzas = document.querySelector("#szorzas")
     szorzas.addEventListener("click", function() {
-        const eredmeny = document.querySelector(".kijelzo")
-        eredmeny.innerHTML += szorzas.innerHTML
+        const eredmeny = document.querySelector(".kifejezes")
+        if (eredmeny.innerHTML === '') {
+            eredmeny.innerHTML = ""
+        } else {
+            eredmeny.innerHTML += szorzas.innerHTML
+        }
     })
 
     const osztas = document.querySelector("#osztas")
     osztas.addEventListener("click", function() {
-        const eredmeny = document.querySelector(".kijelzo")
-        eredmeny.innerHTML += osztas.innerHTML
+        const eredmeny = document.querySelector(".kifejezes")
+        if (eredmeny.innerHTML === '') {
+            eredmeny.innerHTML = ""
+        } else {
+            eredmeny.innerHTML += osztas.innerHTML
+        }
     })
 
     const pont = document.querySelector("#pont")
     pont.addEventListener("click", function() {
-        const eredmeny = document.querySelector(".kijelzo")
-        eredmeny.innerHTML += pont.innerHTML
+        const eredmeny = document.querySelector(".kifejezes")
+        if (eredmeny.innerHTML === '') {
+            eredmeny.innerHTML = ""
+        } else {
+            eredmeny.innerHTML += pont.innerHTML
+        }
     })
 
     const egyenlo = document.querySelector("#egyenlo")
     egyenlo.addEventListener("click", function() {
-        const eredmeny = document.querySelector(".kijelzo")
-        eredmeny.innerHTML += egyenlo.innerHTML
+        const eredmeny = document.querySelector(".kifejezes")
+        if (eredmeny.innerHTML === '') {
+            eredmeny.innerHTML = ""
+        } else {
+            eredmeny.innerHTML += egyenlo.innerHTML
+        }
     })
 
     const torles = document.querySelector("#torles")
     torles.addEventListener("click", function() {
-        const eredmeny = document.querySelector(".kijelzo")
+        const eredmeny = document.querySelector(".kifejezes")
         eredmeny.innerHTML = ""
     })
 }
